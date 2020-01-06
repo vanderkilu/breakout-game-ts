1d7585474e73370c8e29d58b0b8c6f38a4264644
vanderkilu <vanderkiliku@gmail.com>
2020-01-06 21:37:08 +0000

    create live

---
+++ src/entities/Live.ts
@@ -0,0 +1,30 @@
+import {Entity} from './Entity'
+
+
+export class Live extends Entity {
+    private _lives: number = 3
+    constructor(
+        public x: number,
+        public y: number,
+        public width: number,
+        public height: number,
+        public color: string
+    ) {
+        super(x,y,width,height)
+    }
+
+    public draw(ctx: CanvasRenderingContext2D) {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = this.color;
+        ctx.fillText(`LIVES: ${this._lives}`, this.x, this.y);
+    }
+
+    get lives(): number {
+        return this._lives
+    }
+
+    set lives(count: number) {
+        this._lives = count
+    }
+
+}
