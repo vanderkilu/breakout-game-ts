a3ee8e93ca776467cc6d27d7eddfb750bd06f9d4
vanderkilu <vanderkiliku@gmail.com>
2020-01-06 01:28:42 +0000

    calculate ball bounce over paddle direction

--- src/entities/Ball.ts
+++ src/entities/Ball.ts
@@ -5,6 +5,7 @@ import {Paddle} from './Paddle'
 export class Ball extends Entity {
     public dx: number = 2
     public dy: number = -2
+    public speed: number = 3
 
     constructor(
         public x: number,
@@ -28,16 +29,25 @@ export class Ball extends Entity {
         this.y += this.dy
     }
     public checkCollision(gameArea: IGameOptions, paddle: Paddle): void {
-        if ((this.x + this.dx > gameArea.canvasWidth - this.width ) ||
-          (this.x + this.dx  < this.width)){
+        if ((this.x + this.width > gameArea.canvasWidth) ||
+          (this.x - this.width  < 0)){
               this.dx = -this.dx
         }
-        else if (this.y + this.dy < this.width) {
+        else if (this.y - this.width < 0) {
             this.dy = -this.dy
         }
-        else if (this.y + this.dy > gameArea.canvasHeight-this.height) {
-            if(this.x > paddle.x && this.x < paddle.x + paddle.width) {
-                this.dy = - this.dy;
+        else if (this.y + this.width > gameArea.canvasHeight) {
+            if((this.y > paddle.y && this.y < paddle.y + paddle.height) &&
+               (this.x > paddle.x && this.x < paddle.x + paddle.width)) {
+                   //collision position is done such that it always gives us
+                   // a point between 1 and -1. By that we can easy add the angle
+                   // the ball should move if it hits certain positions of the paddle.
+                   let collisionPosition: number = this.x - (paddle.x + paddle.width /2)
+                   collisionPosition = (collisionPosition / paddle.width/2) 
+                   const angle: number = 1 * collisionPosition * Math.PI / 3 // 60deg
+                   //calculate new position of the ball based on angle
+                   this.dx = Math.sin(angle) * this.speed
+                   this.dy = -1 * Math.cos(angle) *  this.speed
             }
         }
     }
